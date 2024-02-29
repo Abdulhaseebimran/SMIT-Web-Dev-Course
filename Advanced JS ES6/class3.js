7d6cd03af5231a44e42efa7cf8a673cf35e3e034
Abdulhaseebimran <haseebimran0001@gmail.com>
2024-02-29 16:10:16 +0500

    ternary operators

---
+++ Advanced JS ES6/class3.js
@@ -0,0 +1,46 @@
+let firstName = "Abdul";
+
+const myobj = {
+    firstName,
+    lastName: "Haseeb",
+    ["class" + "name"]: "React",
+}
+
+// console.log(myobj);
+
+// ternary operator
+
+// let age = 20;
+
+// let isEiligible = age > 18 ? "Yes" : "No";
+
+// console.log(isEiligible);
+
+// let num = 100_00_000_000_00;
+
+// console.log(num);
+
+// let userInput = prompt("Enter your name");
+
+// let name = userInput === "" ? "Guest" : userInput;
+
+// console.log(name);
+
+
+// callBackFunction 
+
+// function greet(call) {
+//     console.log("Hello");
+//     function call() {
+//         console.log("I am a callback function");
+//     }
+//     call();
+// }
+
+// console.log(greet());
+
+
+
+
+
+
